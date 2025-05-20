2437b24b537a4a4ee137d06ae9976d93e9fc12f1
zd <yobaci@163.com>
2025-05-20 18:09:37 +0800

    fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,9 +1,12 @@
+import BaseSprite from '~/components/BaseSprite'
 import NavBar from '~/components/NavBar'
 
 export default function About() {
   return (
     <>
       <NavBar title="关于我们" />
+      <BaseSprite texture="Panda" y={300} x={50} />
+      <BaseSprite texture="Rabbit" y={500} x={50} />
     </>
   )
 }
