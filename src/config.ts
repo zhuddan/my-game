4fcdac613cd7d65e0e69cceddf289c418e010e9d
zd <yobaci@163.com>
2025-05-30 17:01:02 +0800

    123

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,15 @@ export const GameConfig = {
    */
   gameTargetRadius: 200,
   /**
-   *
+   * 箭高度
    */
   arrowHeight: 200,
+  /**
+   * 箭宽度
+   */
   arrowWidth: 8,
+  /**
+   * 箭把半径
+   */
   arrowBallRadius: 20,
 } as const
